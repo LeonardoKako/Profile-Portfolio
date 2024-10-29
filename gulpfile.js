dafb0754ebda6d8bdd5a937beaf1c20be5157a96
LeonardoKako <fleithkako@gmail.com>
2024-10-29 12:15:19 -0300

    Commit quase final

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +14,8 @@ function styles() {
     .pipe(gulp.dest('./dist/css'))
 }
 
-exports.default = gulp.parallel(styles);
+exports.default = gulp.parallel(styles, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 };
